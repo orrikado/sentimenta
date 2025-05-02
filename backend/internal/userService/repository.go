e422e3eb8ce37e919bb1e27c26aa77983d712179
orriginalo <originalo1717@gmail.com>
2025-05-02 21:12:20 +0400

    added user service

--- backend/internal/userService/repository.go
+++ backend/internal/userService/repository.go
@@ -3,22 +3,34 @@ package userservice
 import "gorm.io/gorm"
 
 type UserRepository interface {
-	GetUser(id string) (User, error)
 	CreateUser(user User) error
-	UpdateUser() error
-	DeleteUser() error
+	GetUser(id string) (User, error)
+	UpdateUser(user User) error
+	DeleteUser(id string) error
 }
 
 type userRepository struct {
 	db *gorm.DB
 }
 
-func NewRepository(db *gorm.DB) *UserRepository {
-	return &userRepository{db: db}
-}
-
 func (r *userRepository) GetUser(id string) (User, error) {
 	var user User
 	err := r.db.First(&user, "id = ?", id).Error
 	return user, err
+}
+
+func (r *userRepository) CreateUser(user User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) UpdateUser(user User) error {
+	return r.db.Save(&user).Error
+}
+
+func (r *userRepository) DeleteUser(id string) error {
+	return r.db.Delete(&User{}, "id = ?", id).Error
+}
+
+func NewRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
 }
--- backend/internal/userService/service.go
+++ backend/internal/userService/service.go
@@ -1,1 +1,71 @@
 package userservice
+
+import (
+	"errors"
+	"sentimenta/internal/utils"
+)
+
+type UserService interface {
+	CreateUser(username string, email string, password string) (User, error)
+	GetUser(id string) (User, error)
+	UpdateUser(id string, username string, email string, password string) (User, error)
+	DeleteUser(id string) error
+}
+
+type userService struct {
+	repo UserRepository
+}
+
+func (s *userService) CreateUser(username string, email string, password string) (User, error) {
+	if !utils.IsValidEmail(email) {
+		return User{}, errors.New("email не прошел валидацию")
+	}
+
+	passwordHash := "" // !!! Сделать хеширование
+
+	newUser := User{
+		Username:     username,
+		Email:        email,
+		PasswordHash: passwordHash,
+	}
+	if err := s.repo.CreateUser(newUser); err != nil {
+		return User{}, err
+	}
+	return newUser, nil
+}
+
+func (s *userService) GetUser(id string) (User, error) {
+	return s.repo.GetUser(id)
+}
+
+func (s *userService) UpdateUser(id string, username string, email string, password string) (User, error) {
+	user, err := s.repo.GetUser(id)
+	if err != nil {
+		return User{}, err
+	}
+
+	passwordHash := "" // !!! Сделать хеширование
+
+	if username != "" {
+		user.Username = username
+	}
+	if email != "" {
+		user.Email = email
+	}
+	if password != "" {
+		user.PasswordHash = passwordHash
+	}
+	if err := s.repo.UpdateUser(user); err != nil {
+		return User{}, err
+	}
+	return user, nil
+
+}
+
+func (s *userService) DeleteUser(id string) error {
+	return s.repo.DeleteUser(id)
+}
+
+func NewService(r UserRepository) UserService {
+	return &userService{repo: r}
+}
