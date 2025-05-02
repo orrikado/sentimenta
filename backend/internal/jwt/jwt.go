380edb3147a60cb265c9a7bf0246fbfe4e254435
orriginalo <originalo1717@gmail.com>
2025-05-03 02:21:58 +0400

    added jwt generator&parser pkg

--- backend/internal/jwt/jwt.go
+++ backend/internal/jwt/jwt.go
@@ -1,11 +1,14 @@
 package jwt
 
 import (
+	"errors"
 	cfg "sentimenta/internal/config"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
+
+var jwtKey = cfg.Settings.JWT_SECRET
 
 func GenerateJWT(userID string) (string, error) {
 	claims := jwt.MapClaims{
@@ -17,4 +20,25 @@ func GenerateJWT(userID string) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(cfg.Settings.JWT_SECRET))
+}
+
+func ParseJWT(tokenStr string) (string, error) {
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		// Проверка метода подписи
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("неподдерживаемый метод подписи")
+		}
+		return jwtKey, nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if uid, ok := claims["user_id"].(string); ok {
+			return uid, nil
+		}
+	}
+
+	return "", errors.New("user_id не найден в токене")
 }
