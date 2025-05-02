ab868827556930e330f4fb533202a4d817c41132
orriginalo <originalo1717@gmail.com>
2025-05-03 02:24:36 +0400

    updated main.go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"sentimenta/internal/config"
+	"sentimenta/internal/db"
+	"sentimenta/internal/handlers"
+	"sentimenta/internal/moodService"
+	"sentimenta/internal/userService"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	cfg := config.NewConfig()
+	db := db.InitDB(cfg)
+
+	userRepo := userService.NewRepository(db)
+	userService := userService.NewService(userRepo)
+
+	moodRepo := moodService.NewRepository(db)
+	moodService := moodService.NewService(moodRepo)
+
+	userHandler := handlers.NewUserHandler(userService)
+	authHandler := handlers.NewAuthHandler(userService, *cfg)
+	moodHandler := handlers.NewMoodHandler(moodService, *cfg)
+
+	e := echo.New()
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.POST("/api/auth/login", authHandler.Login)
+	e.POST("/api/auth/register", authHandler.Register)
+
+	e.GET("/api/user/get", userHandler.GetUser)
+	e.PATCH("/api/user/update", userHandler.PatchUpdateUser)
+	e.PUT("/api/user/update/password", userHandler.PutUpdatePasswordUser)
+
+	e.POST("/api/moods/add", moodHandler.PostAddMood)
+	e.GET("/api/moods/get", moodHandler.GetMoods)
+	e.PUT("/api/moods/update", moodHandler.PutUpdateMood)
+
+	e.Logger.Fatal(e.Start("0.0.0.0:8080"))
+}
