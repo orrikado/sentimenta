cd39bbcb670cf5f1dde04bde1ec1f79d4af18911
Vladimir Rubin <vavakado@proton.me>
2025-05-10 01:55:31 +0300

    style(frontend): google login button

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -166,22 +166,16 @@
 				<p class="text-sm text-red-500 dark:text-red-400">{formError}</p>
 			{/if}
 		</form>
-
 		<button
 			type="button"
-			class="mt-4 flex flex-row gap-1.5 border border-black px-4 py-2 text-black hover:bg-black hover:text-white dark:border-white dark:text-white dark:hover:bg-white dark:hover:text-black"
+			class="flex flex-row items-center gap-2 border border-black bg-stone-100 px-4 py-2 text-black transition-none hover:bg-black hover:text-white dark:border-white dark:bg-stone-950 dark:text-white dark:hover:bg-white dark:hover:text-black"
 			on:click={handleGoogleLogin}
 		>
-			<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+			<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 30 30">
 				<defs>
 					<style>
 						.color {
-							fill: #141416;
-						}
-						@media (prefers-color-scheme: dark) {
-							.color {
-								fill: #fff;
-							}
+							fill: currentColor;
 						}
 					</style>
 				</defs>
@@ -190,7 +184,7 @@
 					d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z"
 				></path>
 			</svg>
-			Login with Google
+			<span class="text-sm">Login with Google</span>
 		</button>
 	</main>
 </div>
